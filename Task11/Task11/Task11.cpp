cf05f8961d577ff0f6959494e0e6ceaabf89e6d8
Veronika <VeronikaMa@github.com>
2019-09-04 15:59:06 +0500

    task1 izmenen zadanie verno

---
+++ Task11/Task11/Task11.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int c=0;
+	char *a= new char [1000];
+	cin.getline(a, 1000);
+	while (*a != 0) {
+		if (*a >= 'a' && *a <= 'z')* a -= 32;
+		else if (*a >= 'A' && *a <= 'Z')* a += 32;
+		a++;
+		c++;
+	}
+	a = a - c;
+	cout << a;
+}
