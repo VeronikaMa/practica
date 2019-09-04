dcb2bfe577f1d8a43b66d1b2a75d16d234a7bfa1
Veronika <VeronikaMa@github.com>
2019-09-04 15:01:25 +0500

    task1 add

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -1,20 +1,18 @@
-#include <iostream>
-
+﻿#include <iostream>
 using namespace std;
-
 int main()
 {
 	int a[10];
 	int c = 0, b = 0;
 	for (int i = 0; i < 10; i++)
 	{
-		cin << a[i];
+		cin >> a[i];
 	}
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		if (a[i] % 2 == 0) c++;
 		else b++;
 	}
-	cout >> c >> endl;
-	cout >> b >> endl;
+	cout << c << endl;
+	cout << b << endl;
 }
